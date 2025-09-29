976f4c0fa50a522a2fd9491dc545dd11f6c85d44
tks.samy@gmail.com <tks.samy@gmail.com>
2025-09-29 17:21:14 +0900

    練習

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,24 @@ return (
   <>
     {/* Hello*/}
     <section className="pt-36 pb-24 bg-stone-50">
-      <div className="container mx-auto flex flex-col items-center space-y-8">
-        <div className="h-16 w-16 bg-amber-100 border"></div>
+      <div className="container mx-auto flex flex-col items-center text-center space-y-8">
+        <div className="h-16 w-16 bg-amber-100 border border-stone-200 rounded-full flex justify-center items-center text-3xl">
+          👋
+        </div>
+        <h1 className="text-7x1 font-bold text-stone-800">
+          Welcome to My Portfolio
+        </h1>
+        <p className="text-xl text-stone-500">
+          React と Tailwind CSS が好きな、初心者プログラマです！
+        </p>
+        <a 
+          className="bg-emerald-600 py-3 px-6 text-white flex item-center rounded-lg hover:bg-emerald-700" 
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
       </div>
     </section>
     </>
