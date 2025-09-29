d2c7d0d45729e309255331c37db57f27834aa79a
tks.samy@gmail.com <tks.samy@gmail.com>
2025-09-29 14:03:19 +0900

    テンプレート

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ return (
     {/* Hello*/}
     <section className="pt-36 pb-24 bg-stone-50">
       <div className="container mx-auto flex flex-col items-center space-y-8">
-        <div className="bg-amber-100 border"></div>
+        <div className="h-16 w-16 bg-amber-100 border"></div>
       </div>
     </section>
     </>
 )  
 }
+
+export default App
